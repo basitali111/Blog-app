19dbc0ce3d477df9b10120535af29641543aa9c8
basitali111 <allahbadshah148@gmail.com>
2022-11-16 18:33:09 +0500

    Adding revert

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,41 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  # index
-  describe 'GET #index' do
-    before(:example) do
-      get '/users'
-    end
-
-    it 'is a success' do
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders index template' do
-      expect(response).to render_template('index')
-    end
-
-    it 'renders placeholder for the response body' do
-      expect(response.body).to include('Here is a list of all users')
-    end
-  end
-
-  # show
-  describe 'GET show' do
-    before(:example) do
-      get '/users/1'
-    end
-
-    it 'is a success' do
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders index template' do
-      expect(response).to render_template('show')
-    end
-
-    it 'renders placeholder for the response body' do
-      expect(response.body).to include('Here is a list of one user fetched by their id')
-    end
+  describe 'GET /index' do
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end
